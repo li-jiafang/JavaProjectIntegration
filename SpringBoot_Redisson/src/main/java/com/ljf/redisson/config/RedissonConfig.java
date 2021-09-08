f9809adb13cb9e33c50f85ba30c2bd5ac1cf071d
ljf <ljfsunlight@gmail.com>
2021-09-08 13:27:06 +0800

    增加redisson包,创建RedissonClient连接

---
+++ SpringBoot_Redisson/src/main/java/com/ljf/redisson/config/RedissonConfig.java
@@ -0,0 +1,34 @@
+package com.ljf.redisson.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author: ljf
+ * @Create: 2021/9/8 13:18
+ * @Description:
+ **/
+
+@Configuration
+public class RedissonConfig {
+
+
+    /**
+     * 所有对Redisson的使用都是通过RedissonClient对象来建立连接的
+     * @return
+     */
+    public RedissonClient redisson() {
+        // 默认连接地址 127.0.0.1:6379
+        // 创建配置
+        Config config = new Config();
+        config.useSingleServer().setAddress("myredisserver:6379");
+
+        // 根据配置创建RedissonClient实例
+        RedissonClient redisson = Redisson.create(config);
+        return redisson;
+    }
+
+
+}
