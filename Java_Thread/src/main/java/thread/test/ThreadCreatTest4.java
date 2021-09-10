f19ff133c326d4abd9758562a6360973682e3f0f
ljf <ljfsunlight@gmail.com>
2021-09-10 11:13:57 +0800

    使用FutureTask和Callable来完成线程的创建

---
+++ Java_Thread/src/main/java/thread/test/ThreadCreatTest4.java
@@ -0,0 +1,30 @@
+package thread.test;
+
+import thread.creat.Callable4;
+import thread.creat.Runnable2;
+
+import java.util.concurrent.FutureTask;
+
+/**
+ * @Author: ljf
+ * @Create: 2021/9/10 11:11
+ * @Description:
+ *
+ * 使用Callable+FutureTask来创建线程
+ **/
+public class ThreadCreatTest4 {
+
+    public static void main(String[] args) {
+
+        System.out.println("开始执行方法:");
+
+        FutureTask<Integer> integerFutureTask = new FutureTask<>(new Callable4());
+        new Thread(integerFutureTask).start();
+
+
+        for (int i = 0; i < 100; i++) {
+            System.out.println("主线程:"+Thread.currentThread().getName()+i+"开始执行");
+        }
+
+    }
+}
