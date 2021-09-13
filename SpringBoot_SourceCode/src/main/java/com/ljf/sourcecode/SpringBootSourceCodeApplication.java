36f9d543a308155527b69353a34acc47d3367108
ljf <ljfsunlight@gmail.com>
2021-09-13 22:14:19 +0800

    初始化项目SpringBoot_SourceCode,用来解读SpringBoot源码的项目

---
+++ SpringBoot_SourceCode/src/main/java/com/ljf/sourcecode/SpringBootSourceCodeApplication.java
@@ -0,0 +1,13 @@
+package com.ljf.sourcecode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSourceCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSourceCodeApplication.class, args);
+    }
+
+}
---
+++ SpringBoot_SourceCode/src/test/java/com/ljf/sourcecode/SpringBootSourceCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ljf.sourcecode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSourceCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
